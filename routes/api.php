72262cd96030cb5ad887e8c3a925c7ca00a443fe
Justin Shine <justin.m.shine@gmail.com>
2025-05-29 15:26:54 +0100

    Add controller with functions to register for an api key to use protected endpoints

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TasksController;
+use App\Http\Controllers\UserAuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -16,3 +17,8 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::put('/tasks/{id}', [TasksController::class, 'update'])->name('tasks.update');
     Route::delete('/tasks/{id}', [TasksController::class, 'destroy'])->name('tasks.destroy');
 });
+
+Route::post('register',[UserAuthController::class,'register']);
+Route::post('login',[UserAuthController::class,'login']);
+Route::post('logout',[UserAuthController::class,'logout'])
+  ->middleware('auth:sanctum');
